958159af161fb8d1125cfe0840bf984d905e6f96
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-03-27 20:27:17 +0100

    update app.js
    update app.js

--- app.js
+++ app.js
@@ -3,11 +3,25 @@
     let buttons = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
-    
+    //function for button calls
     buttons.forEach(function(button){
-        button.addEventListener('click', fucntion(e){
+        button.addEventListener('click', function(e){
             let value = e.target.dataset.num;
-            
+            screen.value += value;
         })
+    });
+    //function for equal button
+    equal.addEventListener('click', function(e){
+        if(screen.value === ''){
+            screen.value = "Please enter";
+        } else {
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
     })
+    //function for clear button
+    clear.addEventListener('click', function(e){
+        screen.value = "";
+
+    }
 })();
