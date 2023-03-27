7d5a24c89bbca42db3e41c811c670f2891dd07b4
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-03-27 20:54:37 +0100

    update app.js
    update app.js

--- app.js
+++ app.js
@@ -13,15 +13,11 @@
     //function for equal button
     equal.addEventListener('click', function(e){
         if(screen.value === ''){
-            screen.value = "Please enter";
+            screen.value = "";
         } else {
             let answer = eval(screen.value);
             screen.value = answer;
         }
     })
-    //function for clear button
-    clear.addEventListener('click', function(e){
-        screen.value = "";
-
-    }
+    
 })();
